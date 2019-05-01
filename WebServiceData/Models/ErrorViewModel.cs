4d8402141f55b24bec19641555385101c3716673
Mihai Tirtara <mihai.tirtara@gmail.com>
2019-05-01 11:31:47 +0200

    Start

---
+++ WebServiceData/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebServiceData.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
